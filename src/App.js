35a2319db5fda5006a45a3170b9bd72c6d842052
Bosi Chen <chenbosi.bosi@gmail.com>
2023-09-09 12:00:30 -0400

    index page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import ShortIntro from './components/ShortIntro';
+import ProjectPage from './components/projectpage/Projectpage';
+import useVisualMode from './hooks/useVisualHook';
 import './App.css';
-import Nav from './pages/Nav';
+import Skills from './components/Skills';
 
 function App() {
+  const INTRO = 'INTRO';
+  const SKILLS = 'SKILLS';
+  const PROJECTS = 'PROJECTS';
+  const MORE = 'MORE';
+  const { mode, transition } = useVisualMode(INTRO);
+
+  const select = (event) => {
+    transition(event);
+  }
+
+  
   return (
     <div className="App">
-      <Nav />
+      <Nav select={select} />
+      {mode === INTRO && <ShortIntro />}
+      {mode === SKILLS && <Skills />}
+      {mode === PROJECTS && <ProjectPage />}
+      {mode === MORE}
+      <Footer />
     </div>
   );
 }

a36422abf88168142d5919766d9d81c730fafb0d
Bosi Chen <chenbosi.bosi@gmail.com>
2023-09-09 12:02:40 -0400

    hooks

---
+++ src/hooks/useVisualHook.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // Change to a new mode
+  const transition = (newMode, replace = false) => {
+    if (replace && history.length > 1) {
+      setHistory(prev => prev.slice(0, -1));
+    }
+    setHistory(prev => ([...prev, newMode]));
+    setMode(newMode);
+  };
+
+  // Back to the old mode
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => prev.slice(0, -1));
+    }
+  };
+
+  return { mode, transition, back };
+};

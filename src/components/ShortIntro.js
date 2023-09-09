96f88e05d71ea10230ea002689c52cd2e8789d99
Bosi Chen <chenbosi.bosi@gmail.com>
2023-09-09 12:02:28 -0400

    intro and skills

---
+++ src/components/ShortIntro.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./intro.scss";
+
+
+export default function ShortIntro () {
+  return (
+    <main>
+      <div className="profile">
+        <div className="bg-img"></div>
+        <div id="title">
+          <p id="greeting">Hello, my name is</p>
+          <p id="name">BOSI CHEN</p>
+          <p id="bio">A Junior Full Stack Web Developer</p>
+          </div>
+      </div>
+      <div className="intro-text" id="intro-text">
+        <p className="content">An enthusiastic Full Stack Developer.</p>
+        <p className="content">Skilled with JavaScript, React.js, HTML and CSS front-end developing. Also strong in Node.js and Express.js backend building techniques.</p>
+        <p className="content">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p>
+        <p id="more"><a id="more" href="/more-about-me">more...</a></p>
+      </div>
+    </main>
+  );
+};
---
+++ src/components/Skills.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./skills.scss";
+
+
+export default function Skills() {
+  return (
+    <main>
+      <h2>My Skills</h2>
+      <div className="skills">
+        <h3>Operating Systems</h3>
+        <h3>Languages</h3>
+        <div id="languages" className="sorts">
+          <img className="stack-img" alt="js" src="https://drive.google.com/uc?export=view&id=1MDMQaRCt7KvD6gbN-6Fe0Y_5rw-sdR_e" />
+          <img className="stack-img" alt="python" src="https://drive.google.com/uc?export=view&id=1LuC9FqQtI5apXHIPUmsXZ2R1ohMCeFMT" />
+        </div>
+        <h3>Back End</h3>
+        <div id="back-end" className="sorts">
+          <img className="stack-img" alt="node" src="https://drive.google.com/uc?export=view&id=1MGvn5vfXONSYBkV4CXE4iSJkDO8EXiXc" />
+          <img className="stack-img" alt="express" src="https://drive.google.com/uc?export=view&id=1MfxzExsy3gbt6xH3Y_CN6R63Hc4qyL49" />
+          <img className="stack-img" alt="postgres" src="https://drive.google.com/uc?export=view&id=1MgnQXlUMqAxEeYOZ6bLoIUDs3c6xSw7P" />
+          <img className="stack-img" alt="jquery" src="https://drive.google.com/uc?export=view&id=1MwIo42zeCy1DaGmhZlQZ9b6wWsuK5skU" />
+          <img className="stack-img" alt="ejs" src="https://drive.google.com/uc?export=view&id=1Mkx0xG7oTTOSn4CMSIeLgI3z-HD5BFIP" />
+          <img className="stack-img" alt="ruby" src="https://drive.google.com/uc?export=view&id=1MpX5EEe84wNTW4j56IdReOV_7lsfh500" />
+          <img className="stack-img" alt="ruby on rails" src="https://drive.google.com/uc?export=view&id=1MtZphWX-BOVltJ9l0b1EmZOkoTt9BCHL" />
+        </div>
+        <h3>Front End</h3>
+        <div id="front-end" className="sorts">
+          <img className="stack-img" alt="react" src="https://drive.google.com/uc?export=view&id=1MK4tg91CV7owfy8mwou-NSLpLJs_p68L" />
+          <img className="stack-img" alt="html" src="https://drive.google.com/uc?export=view&id=1MS_vIXrac-LZlVOg4PTBZz9jAoc9Orfm" />
+          <img className="stack-img" alt="css" src="https://drive.google.com/uc?export=view&id=1MdtSfROQsgumyXMHQx7HKhDmVRJ6igix" />
+          <img className="stack-img" alt="scss" src="https://drive.google.com/uc?export=view&id=1ddZ7-57SOZaH4xQ6s7w29FhPw6WNNeUl" />
+          <img className="stack-img" alt="bootstrap" src="https://drive.google.com/uc?export=view&id=1gGz2sCIVZfQWrcAcKjcZmdaiV8_Jmbqy" />
+        </div>
+        <h3>Testing</h3>
+        <div id="testing" className="sorts">
+          <img className="stack-img" alt="mocha chai" src="https://drive.google.com/uc?export=view&id=1RDkoGxbc6dWyYgObNdzzF-fDIEmr2wgk" />
+          <img className="stack-img" alt="jest" src="https://drive.google.com/uc?export=view&id=1LsRHpBIiu_nnFiAnqHiBeqedCzp-Zriu" />
+          <img className="stack-img" alt="storybook" src="https://drive.google.com/uc?export=view&id=13OrvusZmqDlgslrth5n8dWuoWFc4HVx3" />
+          <img className="stack-img" alt="cypress" src="https://drive.google.com/uc?export=view&id=1GXivIWDULFKG2Sa5rbgy03CNuJoSdRfe" />
+          <img className="stack-img" alt="rspec" src="https://drive.google.com/uc?export=view&id=1cPZq-8Q6CmLCOPzuuEDefGKdvCHBkICr" />
+        </div>
+        <h3>Soft Skills</h3>
+        <h3>Spoken Languages</h3>
+        <p>- English</p>
+        <p>- Chinese(Mandarin)</p>
+        <p>- Dutch</p>
+        <p>- French(Basic)</p>
+      </div>
+    </main>
+  )
+};

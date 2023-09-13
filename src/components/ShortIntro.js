582e7952ac291a84eedcf6490038367ceb1ced63
Bosi Chen <chenbosi.bosi@gmail.com>
2023-09-13 19:41:41 -0400

    edit short intro

--- src/components/ShortIntro.js
+++ src/components/ShortIntro.js
@@ -15,7 +15,7 @@ export default function ShortIntro () {
       </div>
       <div className="intro-text" id="intro-text">
         <p className="content">An enthusiastic Full Stack Developer.</p>
-        <p className="content">Skilled with JavaScript, React.js, HTML and CSS front-end developing. Also strong in Node.js and Express.js backend building techniques.</p>
+        <p className="content">Skilled with JavaScript, React.js, HTML and CSS front-end developing. Also strong in backend with C# and php. Good at database design and management, and SQL languages.</p>
         <p className="content">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p>
         <p id="more"><a id="more" href="/more-about-me">more...</a></p>
       </div>

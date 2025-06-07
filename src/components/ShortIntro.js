a752aaa4f1f812c59432d9449b6c018ee1468a49
Bosi Chen <chenbosi.bosi@gmail.com>
2025-06-07 02:30:53 -0400

    update starter page

--- src/components/ShortIntro.js
+++ src/components/ShortIntro.js
@@ -9,26 +9,28 @@ export default function ShortIntro () {
       <div className="profile">
         {/* <div className="bg-img"></div> */}
         <div id="title">
-          <p id="greeting">&nbsp;Hello, my name is</p>
-          <p id="name">Bosi&nbsp;&nbsp;Chen</p>
-          {/* <p id="bio">A Junior Full Stack Web Developer</p> */}
-          <p id="bio">&nbsp;A Full Stack Web Developer</p>
+          <p id="greeting">Hello, I'm</p>
+          <p id="name">Bosi Chen</p>
+          <p id="bio">A Full Stack Web Developer</p>
         </div>
         <div id="cover-img">
           <img id="cover" src={coverImg} alt="cover" />
         </div>
       </div>
-      {/* <div className="summary"> */}
-        {/* <div id="summary-img">
+      <div className="contact-me">
+      </div>
+      <div className="summary">
+        <div id="summary-img">
           <img id="summary-image" src={image1} alt="image1" />
-        </div> */}
-        {/* <div className="intro-text" id="intro-text">
+        </div>
+        <h2>About Me</h2>
+        <div className="intro-text" id="intro-text">
           <p className="content">An enthusiastic Full Stack Developer.</p>
-          <p className="content">Skilled with JavaScript, React.js, HTML and CSS front-end developing. Also strong in backend with C# and php. Good at database design and management, and SQL languages.</p>
-          <p className="content">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p> */}
+          <p className="content">Skilled with JavaScript, React.js, HTML and CSS front-end developing. Also strong in backend with C#, php and Python. Good at database design and management, and SQL languages as well.</p>
+          <p className="content">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p>
           {/* <p id="more"><a id="more" href="#nav">more...</a></p> */}
-        {/* </div> */}
-      {/* </div> */}
+        </div>
+      </div>
     </div>
   );
 };
